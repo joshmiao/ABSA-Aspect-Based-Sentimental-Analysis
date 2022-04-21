c7996b5a0c9159ba91aae118af210fdec8ad016c
joshmiao <myk0210@163.com>
2022-04-21 20:09:33 +0800

    update

---
+++ data_utils.py
@@ -0,0 +1,97 @@
+import torch
+from torch.utils.data import DataLoader, Dataset
+
+
+class MyDataset(Dataset):
+    def __init__(self, data):
+        self.data = data
+        self.len = len(data)
+
+    def __getitem__(self, index):
+        return self.data[index][0], self.data[index][1]
+
+    def __len__(self):
+        return self.len
+
+
+def load_data(tokenizer, file_path, batch_size, device):
+    data = []
+    label_dict = {'0': 1, '1': 2, '-1': 3}
+    data_file = open(file_path, 'r')
+    last_text = ''
+    while True:
+        text = data_file.readline()[:-1]
+        target_word = data_file.readline()[:-1]
+        word_label = data_file.readline()[:-1]
+        # print(text)
+        # print(word)
+        # print(word_label)
+        if not text:
+            break
+        assert word_label in ['0', '-1', '1']
+        labels = []
+        for word in text.split(' '):
+            if word == '$T$':
+                tokenized_word = tokenizer.encode(target_word)
+                labels += [label_dict[word_label]] * len(tokenized_word)
+            else:
+                tokenized_word = tokenizer.encode(word)
+                labels += [0] * len(tokenized_word)
+        text = text.replace('$T$', target_word)
+        sentence = tokenizer.encode(text)
+
+        print(len(sentence), len(labels))
+        assert len(sentence) == len(labels) and len(sentence) <= 500 and len(labels) <= 500
+        sentence += [0] * (100 - len(sentence))
+        labels += [0] * (100 - len(labels))
+
+        sentence = torch.tensor(sentence, device=device)
+        labels = torch.tensor(labels, device=device)
+        # print(target_word)
+        # print(text.replace('$T$', target_word))
+        # print(last_text)
+        if text == last_text:
+            data[-1][1] += labels
+        else:
+            data.append([sentence, labels])
+        last_text = text
+    return DataLoader(MyDataset(data), batch_size=batch_size)
+
+
+def load_data_old(tokenizer, file_path, batch_size, device):
+    labels_dic = {'O': 0, 'T-POS': 1, 'T-NEU': 2, 'T-NEG': 3}
+    data = []
+    cnt = 0
+    data_file = open(file_path, 'r')
+    for _, line in enumerate(data_file):
+        line = line.split('####')
+        sentence = tokenizer.encode(line[0])
+        # sentence = tokenizer.encode(line[0])
+        line[1] = line[1][0:-1].split(' ')  # delete the '\n' at the end of each sentence
+        labels = []
+        for word in line[1]:
+            if word.count('=') != 1:
+                continue
+            if not word.split('=')[1] in labels_dic:
+                continue
+            length = len(tokenizer.encode(word.split('=')[0]))
+            labels += [labels_dic[word.split('=')[1]]] * length
+
+        # print(sentence)
+        # print(labels)
+        print(len(sentence), len(labels))
+        assert len(sentence) <= 500 and len(labels) <= 500
+        if len(sentence) != len(labels):
+            print('error at line', _)
+            print(sentence)
+            print(labels)
+            cnt += 1
+        else:
+            sentence += [0] * (500 - len(sentence))
+            labels += [0] * (500 - len(labels))
+            labels = torch.tensor(labels, device=device)
+            sentence = torch.tensor(sentence, device=device)
+            data.append([sentence, labels])
+    print(cnt)
+    # print(data)
+    return DataLoader(MyDataset(data), batch_size=batch_size)
--- main.py
+++ main.py
@@ -1,34 +1,24 @@
-from transformers import BertModel, BertConfig, BertTokenizer, BertForPreTraining, BertForNextSentencePrediction
+from transformers import BertModel, BertConfig, BertTokenizer, BertForPreTraining
+import data_utils
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+import torch.nn.functional as func
 import torch.optim as optim
 
-# model_name = 'bert-base-uncased'
-# tokenizer = BertTokenizer.from_pretrained(model_name)
-# model = BertModel.from_pretrained(model_name)
+device = torch.device('cuda:0')
 
 model_path = './BERT_model/uncased_L-12_H-768_A-12/'
 config = BertConfig.from_json_file(model_path + 'bert_config.json')
 tokenizer = BertTokenizer.from_pretrained(model_path + 'vocab.txt')
 model = BertForPreTraining.from_pretrained(model_path + 'bert_model.ckpt.index', from_tf=True, config=config)
 
-s1 = '[CLS] The [MASK] went to the store.[SEP]'
-s2 = 'Penguins [MASK] flightless birds.[SEP]'
-# s1 = '[CLS]My dog is cute.[SEP]'
-# s2 = 'He likes playing.[SEP]'
-# s1 = '[CLS]My dog is cute.[SEP]'
-# s2 = 'The man went to the store.[SEP]'
-s1 = tokenizer.encode(s1)
-s2 = tokenizer.encode(s2)
-token_type_ids = [0] * len(s1) + [1] * len(s2)
-token_type_ids = torch.tensor([token_type_ids])
-print(token_type_ids)
-input_ids = torch.tensor(s1 + s2).unsqueeze(0)
-print(input_ids)
-outputs = model(input_ids, token_type_ids=token_type_ids)
-print(outputs)
-# last_hidden_states = outputs[0]
-# print(last_hidden_states.size())
-# print(last_hidden_states)
+# data_paths = ['./data/laptop14/', './data/rest14/', './data/rest15/', './data/rest16/']
+data_paths = ['./data/Semeval&Twitter/semeval14/Laptops_Train.xml.seg']
+train_dataset = data_utils.load_data(file_path=data_paths[0], tokenizer=tokenizer, batch_size=5,
+                                    device=device)
+
+for step, data in enumerate(train_dataset):
+    if step >= 1:
+        break
+    print(step, data)
